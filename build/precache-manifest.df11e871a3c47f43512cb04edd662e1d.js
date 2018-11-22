a92f4a9e8776920a40d7f7d915ff5da4f3779994
Oba-One <obaone01@gmail.com>
2018-11-21 21:45:35 -0800

    removed build gitignore

---
+++ build/precache-manifest.df11e871a3c47f43512cb04edd662e1d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6ebacb81662d0258a6a0",
+    "url": "/static/js/main.6ebacb81.chunk.js"
+  },
+  {
+    "revision": "4297b93a58ce7b189090",
+    "url": "/static/js/1.4297b93a.chunk.js"
+  },
+  {
+    "revision": "fb53f447ca986029070c28aeec95c7b2",
+    "url": "/index.html"
+  }
+];
